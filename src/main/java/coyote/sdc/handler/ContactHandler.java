64a669c48fc46e8889fafa0deff7ba6e53aff992
Steve Cote <sdcote@gmail.com>
2017-03-21 21:41:05 -0400

    Use handlers for the contact form

---
+++ src/main/java/coyote/sdc/handler/ContactHandler.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and implementation
+ */
+package coyote.sdc.handler;
+
+import java.util.Map;
+
+import coyote.commons.network.http.IHTTPSession;
+import coyote.commons.network.http.Response;
+import coyote.commons.network.http.auth.Auth;
+import coyote.commons.network.http.handler.UriResource;
+import coyote.commons.network.http.handler.UriResponder;
+
+
+/**
+ * This handles the contact me form.
+ */
+@Auth(required = false)
+public class ContactHandler extends AbstractJsonHandler implements UriResponder {
+
+  /**
+   * 
+   */
+  @Override
+  public Response post( UriResource uriResource, Map<String, String> urlParams, IHTTPSession session ) {
+    // TODO: implement a mailer...maybe a log entry?
+    return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+  }
+
+}
